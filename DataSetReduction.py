72c1a5f24539f51e06e692a663f59439d68dc630
SunnyDjinn <guiol.matt@gmail.com>
2016-02-22 10:34:00 -0500

    Data reduction working now, but don't know how to split the training data to avoid memory overflow yet

--- DataSetReduction.py
+++ DataSetReduction.py
@@ -1,5 +1,14 @@
+import json
+#import pandas 
+
 # the number of data entries being read in
-increment = 500000;
+increment = 100;
+
+# Path where the dataset is stored - MODIFY BEFORE LAUNCHING
+PATH_TO_DATASET = 'rcomments'
+
+# Path where to store the reduced dataset - MODIFY BEFORE LAUNCHING
+PATH_TO_REDUCED_DATASET = './ReducedData'
 
 # get file length
 def file_len(fname):
@@ -11,60 +20,25 @@ def file_len(fname):
 # read in all the data
 def getRawData(filePath):
     rawData = [0 for j in range(increment)];
-    count = 0;
     with open(filePath) as file:
         for i in range(increment):
-            rawData[count] = file.readline();
-            count += 1;
+            rawData[i] = json.loads(file.readline())
     return rawData;
-
-# split the data by commas
-def splitData(rawData):
-    fullData = [[0 for i in range(25)] for j in range(increment)]
-    count = 0;
-    for i in rawData:
-        fullData[count] = i.split(',')
-        # fullData[count] = re.split(r', (?=(?:"[^"]*?(?: [^"]*)*))|, (?=[^",]+(?:,|$))', i)
-        count += 1
-    return fullData
 
 # reduce the data to the proper tags
 # author, created_utc, score, ups, downs, body, subreddit
-def reducedData(fullData):
+def parsedAndreducedData(fullData):
     data = [[0 for m in range(7)]for k in range(increment)]
-    for i in range(len(fullData)):
-        count = 0;
-        for j in fullData[i]:
-            string = j
-            if  string.endswith('\n'):
-                string = string[:-2]
-            if '"author":' in string:
-                data[i][0] = parse(string)
-            elif '"created_utc":' in string:
-                data[i][1] = parse(string)
-            elif '"body":' in string:
-                if string[-1] == '\"':
-                    data[i][2] = parse(string)
-                else:
-                    begin = string.find(":") + 2
-                    data[i][2] = string[begin:] +", "
-                    # pos = (count) + 1
-                    # while ':' not in fullData[i][pos] and fullData[i][pos]!='0' and pos < 25:
-                        # print(fullData[i][pos])
-                        # data[i][2] += parse(fullData[i][pos])
-                        # pos +=1
-            elif '"score":' in string:
-                begin = string.find(":") + 1
-                data[i][3] = string[begin:]
-            elif '"ups":' in string:
-                begin = string.find(":") + 1
-                data[i][4] = string[begin:]
-            elif '"downs":' in string:
-                begin = string.find(":") + 1
-                data[i][5] = string[begin:]
-            elif '"subreddit":' in string:
-                data[i][6] = parse(string)
-            count += 1
+    i = 0
+    for datapoint in fullData:
+        data[i][0] = datapoint['author']
+        data[i][1] = datapoint['created_utc']
+        data[i][2] = datapoint['body'].replace('\n', '')
+        data[i][3] = datapoint['score']
+        data[i][4] = datapoint['ups']
+        data[i][5] = datapoint['downs']
+        data[i][6] = datapoint['subreddit']
+        i += 1
     return data
 
 # get the word from a string
@@ -74,15 +48,14 @@ def parse(string):
 
 # write the data to a file
 def writeDataToFile(data):
-    w = open('D:/RedditDataSetSmall.txt', "a")
-    for i in range(increment):
-        for j in range(7):
-            w.write(str(data[i][j]) + " ")
-        w.write("\n")
+    w = open(PATH_TO_REDUCED_DATASET, "a")
+    for line in data:
+        w.write(json.dumps(line))
+    w.close()
 
-rawData = getRawData('D:/RedditDataSet.txt')
-fullData = splitData(rawData)
-data = reducedData(fullData)
+
+rawData = getRawData(PATH_TO_DATASET)
+data = parsedAndreducedData(rawData)    # Data contains now rows of reduced data from 0 to 6, respectively author, created_utc, body, score, ups, downs, subreddit
+writeDataToFile(data)
 
 print (len(data))
-print (data[0])
