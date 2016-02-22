bffd5c98c15328cd2d862e81366974508233db3f
SunnyDjinn <guiol.matt@gmail.com>
2016-02-22 11:57:24 -0500

    Export to csv added - testing right now

--- DataSetReduction.py
+++ DataSetReduction.py
@@ -1,3 +1,5 @@
+## -*- coding: utf-8 -*-
+
 import json
 #import pandas 
 
@@ -9,6 +11,8 @@ PATH_TO_DATASET = 'rcomments'
 
 # Path where to store the reduced dataset - MODIFY BEFORE LAUNCHING
 PATH_TO_REDUCED_DATASET = './ReducedData'
+
+CSV_OUTPUT_FILE = './ReducedCSV.csv'
 
 # get file length
 def file_len(fname):
@@ -41,6 +45,18 @@ def parsedAndreducedData(fullData):
         i += 1
     return data
 
+def parseAndReduceLine(line):
+    data = [0 for m in range(7)]
+    datapoint = json.loads(line)
+    data[0] = datapoint['author']
+    data[1] = datapoint['created_utc']
+    data[2] = datapoint['body'].replace('\n', '')
+    data[3] = datapoint['score']
+    data[4] = datapoint['ups']
+    data[5] = datapoint['downs']
+    data[6] = datapoint['subreddit']
+    return data
+
 # get the word from a string
 def parse(string):
     begin = string.find(":") + 2
@@ -48,11 +64,29 @@ def parse(string):
 
 # write the data to a file
 def writeDataToFile(data):
-    w = open(PATH_TO_REDUCED_DATASET, "a")
+    w = open(PATH_TO_REDUCED_DATASET, "w")
     for line in data:
         w.write(json.dumps(line))
     w.close()
 
+# exports all input data to parsed and reduced csv 
+def readAllAndExportCSV(inputFilePath, outputFilePath):
+    outputFile = open(outputFilePath, 'w')
+    with open(inputFilePath) as inputFile:
+        for line in inputFile:
+            lineToWrite = ""
+            parsedLine = parseAndReduceLine(line)
+            for value in parsedLine:
+                if type(value) == int:
+                    lineToWrite += "," + str(value)
+                else:
+                    lineToWrite += value.encode('utf-8', 'ignore')
+
+            outputFile.write(lineToWrite + '\n')
+
+
+#readAllAndExportCSV(PATH_TO_DATASET, CSV_OUTPUT_FILE) # ReducedCSV contains the whole file with reduced dimension, colon separated (easier to read with pandas)
+#exit()
 
 rawData = getRawData(PATH_TO_DATASET)
 data = parsedAndreducedData(rawData)    # Data contains now rows of reduced data from 0 to 6, respectively author, created_utc, body, score, ups, downs, subreddit
