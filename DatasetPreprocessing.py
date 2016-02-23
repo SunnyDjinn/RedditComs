b9cf76549c303766b2029d7f540e5cf2899fd67e
SunnyDjinn <guiol.matt@gmail.com>
2016-02-23 08:51:09 -0500

    Implemented function to correct imperfections in the dataset (commas at the end of line)

--- DatasetPreprocessing.py
+++ DatasetPreprocessing.py
@@ -2,5 +2,20 @@ import pandas
 
 PATH_TO_CSV = 'ReducedCSV.csv'
 
+PATH_TO_CORRECTED_CSV = 'CorrectedCSV.csv'
 
-data = pandas.read_csv(PATH_TO_CSV)
+def correctImperfectionsData(inputFilePath, outputFilePath):
+    outputFile = open(outputFilePath, 'w')
+    outputFile.write('username,date,body,score, ups,downs,subreddit\n')
+    with open(inputFilePath) as inputFile:
+        for line in inputFile:
+            lineToWrite = line[:-2]
+            outputFile.write(lineToWrite + '\n')
+
+
+#correctImperfectionsData(PATH_TO_CSV, PATH_TO_CORRECTED_CSV)
+
+#data = pandas.read_csv(PATH_TO_CORRECTED_CSV, header=0) # first row as column title, parses date using column 1
+
+
+#print pandas.to_datetime(data['date'][99], unit='s') # to convert to readable time 
