762251ead1e12b2427a030a373d526e3664e7541
SunnyDjinn <guiol.matt@gmail.com>
2016-02-24 07:52:02 -0500

    Modified for data reduction / character removal and links finding and transformation

--- DataSetReduction.py
+++ DataSetReduction.py
@@ -2,6 +2,8 @@
 
 import json
 #import pandas 
+import string
+import re
 
 # the number of data entries being read in
 increment = 100;
@@ -12,7 +14,7 @@ PATH_TO_DATASET = 'rcomments'
 # Path where to store the reduced dataset - MODIFY BEFORE LAUNCHING
 PATH_TO_REDUCED_DATASET = './ReducedData'
 
-CSV_OUTPUT_FILE = './ReducedCSV.csv'
+CSV_OUTPUT_FILE = './VeryReducedCSV.csv'
 
 # get file length
 def file_len(fname):
@@ -32,29 +34,33 @@ def getRawData(filePath):
 # reduce the data to the proper tags
 # author, created_utc, score, ups, downs, body, subreddit
 def parsedAndreducedData(fullData):
-    data = [[0 for m in range(7)]for k in range(increment)]
+    data = [[0 for m in range(5)]for k in range(increment)]
     i = 0
     for datapoint in fullData:
         data[i][0] = datapoint['author']
         data[i][1] = datapoint['created_utc']
-        data[i][2] = datapoint['body'].replace('\n', '').replace(',', '')   # Replaces carriage return and commas in the body (so no problem when export to csv)
+        data[i][2] = str(datapoint['body']).replace('\n', '').replace(',', '').lower().translate(all, KEEPER)   # Replaces carriage return and commas in the body (so no problem when export to csv)
         data[i][3] = datapoint['score']
-        data[i][4] = datapoint['ups']
-        data[i][5] = datapoint['downs']
-        data[i][6] = datapoint['subreddit']
+        data[i][4] = datapoint['subreddit']
         i += 1
     return data
 
-def parseAndReduceLine(line):
-    data = [0 for m in range(7)]
+def parseAndReduceLine(line): 
+
+    data = [0 for m in range(3)]
     datapoint = json.loads(line)
-    data[0] = datapoint['author']
-    data[1] = datapoint['created_utc']
-    data[2] = datapoint['body'].replace('\n', '').replace(',', '')   # Replaces carriage return and commas in the body (so no problem when export to csv)
-    data[3] = datapoint['score']
-    data[4] = datapoint['ups']
-    data[5] = datapoint['downs']
-    data[6] = datapoint['subreddit']
+
+    if int(datapoint['score']) <= 9:
+        return None 
+
+    #data[0] = datapoint['author']
+    #data[1] = datapoint['created_utc']
+    data[0] = datapoint['body'].replace('\n', '').replace(',', '').lower().encode('utf-8', 'ignore')   # Replaces carriage return and commas in the body (so no problem when export to csv)
+    data[0] = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '[LINK]', data[0])
+
+    data[0] = data[0].translate(all, KEEPER)
+    data[1] = datapoint['score']
+    data[2] = datapoint['subreddit']
     return data
 
 # get the word from a string
@@ -72,18 +78,25 @@ def writeDataToFile(data):
 # exports all input data to parsed and reduced csv 
 def readAllAndExportCSV(inputFilePath, outputFilePath):
     outputFile = open(outputFilePath, 'w')
+    outputFile.write('body,score,subreddit\n')
+
     with open(inputFilePath) as inputFile:
         for line in inputFile:
             lineToWrite = ""
             parsedLine = parseAndReduceLine(line)
+            if parsedLine == None:
+                continue
             for value in parsedLine:
                 if type(value) == int:
                     lineToWrite += str(value) + ","
                 else:
                     lineToWrite += value.encode('utf-8', 'ignore') + ','
 
-            outputFile.write(lineToWrite + '\n')
+            outputFile.write(lineToWrite[:-2] + '\n')
 
+
+all = string.maketrans('','')
+KEEPER = all.translate(all, string.digits + string.lowercase + ',!? .()[]\'')
 
 readAllAndExportCSV(PATH_TO_DATASET, CSV_OUTPUT_FILE) # ReducedCSV contains the whole file with reduced dimension, colon separated (easier to read with pandas)
 exit()
@@ -93,3 +106,8 @@ data = parsedAndreducedData(rawData)    # Data contains now rows of reduced data
 writeDataToFile(data)
 
 print (len(data))
+
+
+# stop words
+# symbols
+# to lower 
--- DatasetPreprocessing.py
+++ DatasetPreprocessing.py
@@ -1,4 +1,5 @@
 import pandas
+import string
 
 PATH_TO_CSV = 'ReducedCSV.csv'
 
@@ -6,16 +7,37 @@ PATH_TO_CORRECTED_CSV = 'CorrectedCSV.csv'
 
 def correctImperfectionsData(inputFilePath, outputFilePath):
     outputFile = open(outputFilePath, 'w')
-    outputFile.write('username,date,body,score, ups,downs,subreddit\n')
+    outputFile.write('username,date,body,score,subreddit\n')
     with open(inputFilePath) as inputFile:
         for line in inputFile:
-            lineToWrite = line[:-2]
-            outputFile.write(lineToWrite + '\n')
+            outputFile.write(lineToWrite)
 
 
 #correctImperfectionsData(PATH_TO_CSV, PATH_TO_CORRECTED_CSV)
 
-#data = pandas.read_csv(PATH_TO_CORRECTED_CSV, header=0) # first row as column title, parses date using column 1
+
+#data = pandas.read_csv(PATH_TO_CSV, header=0) # first row as column title, parses date using column 1
+#print data
+
+#outputFile = open('reducedSmall.csv', 'w')
+
+all = string.maketrans('','')
+keeper = all.translate(all, string.digits + string.lowercase + ',!? .()[]\'')
+
+tp = pandas.read_csv('smallDataTest.csv', header=0, iterator=True, chunksize=1)
+for chunk in tp:
+	for body in chunk['body']:
+		body = str(body).lower() #datapoint[2] = 
+		body = body.translate(all, keeper)
+	chunk.to_csv(path_or_buf=stringToPrint)
+	print 
+	#outputFile.write(chunk.to_csv())
+
+
+
+exit()
+
+print df
 
 
 #print pandas.to_datetime(data['date'][99], unit='s') # to convert to readable time 
